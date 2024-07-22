c17d0a0b7d1e3c55d9928b170efd5f0aab180728
TAPANANSHU TRIPATHY <166393966+TAPANANSHUTRIPATHY@users.noreply.github.com>
2024-07-22 14:40:02 +0530

    Add files via upload

---
+++ 3_Temperature Converter Website/func.js
@@ -0,0 +1,26 @@
+const inputs = document.querySelectorAll(".panel input");
+const c = document.querySelector("#celsius");
+const f = document.querySelector("#farenheit");
+const k = document.querySelector("#kelvin");
+
+inputs.forEach(input => {
+    input.addEventListener("input", e => {
+        const unit = e.target.id;
+        const v = parseInt(e.target.value);
+
+        if (unit === "celsius") {
+            f.value = parseFloat((v * 1.8) + 32).toFixed(3) * 1;
+            k.value = parseFloat(v + 273.15).toFixed(3) * 1;
+        }
+
+        else if (unit === "farenheit") {
+            c.value = parseFloat((v - 32) * 5 / 9).toFixed(3) * 1;
+            k.value = parseFloat(((v - 32) * 5 / 9) + 273.15).toFixed(3) * 1;
+        }
+
+        else if (unit === "kelvin") {
+            c.value = parseFloat(v - 273.15).toFixed(3) * 1;
+            f.value = parseFloat((v - 273.15) * 9 / 5 + 32).toFixed(3) * 1;
+        }
+    });
+});
